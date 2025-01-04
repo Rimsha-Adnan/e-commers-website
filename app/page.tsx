2688c8f4d3c8119a4a6c296a79d75b2e24adc114
rimsha-adnan <adnanhuzaifa@gmail.com>
2025-01-04 18:25:33 +0500

    making chnages

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import HeroSection from "./components/HeroSection";
 import ProductCard from "./components/ProductCard";
 import Products from "./components/Products";
 import Reviews from "./components/Reviews";
-import FooterLast from "./components/FooterLast";
-import Footer from "./components/Footer";
 
 export default function Home() {
   return (
